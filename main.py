9a68a8499d20341737e9776d4ae6eef34bbae73c
KEV\KelvinKev <ngominhthanh99@gmail.com>
2025-02-16 03:36:33 +0700

    bulk test, close position

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.login import login
 from src.tests.test_market_order import test_place_market_order
 from src.tests.test_limit_order import test_place_limit_order
 from src.tests.test_stop_order import test_place_stop_order
+from src.tests.test_close_position import test_close_position
+from src.tests.test_bulk_close import test_bulk_close
 import time
 
 def main():
@@ -14,16 +16,24 @@ def main():
         login(driver)
         time.sleep(5)
 
-        # Test market order
-        test_place_market_order(driver)
+        #Test bulk clsoe
+        test_bulk_close(driver)
         time.sleep(5)
 
-        # Test limit order
-        test_place_limit_order(driver)
-        time.sleep(5)
+        # # Test close position
+        # test_close_position(driver)
+        # time.sleep(5)
 
-        # Test stop order
-        test_place_stop_order(driver)
+        # # Test market order
+        # test_place_market_order(driver)
+        # time.sleep(5)
+
+        # # Test limit order
+        # test_place_limit_order(driver)
+        # time.sleep(5)
+
+        # # Test stop order
+        # test_place_stop_order(driver)
 
     except Exception as e:
         print(f"Unexpected error in main execution: {e}")
